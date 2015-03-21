ac07630ad7febb3cafd95e0016871a83ac9e566d
Lars Tveito <larstvei@ifi.uio.no>
2015-03-21 04:05:42 +0100

    Added solution to wordlist.

--- wordlist.clj
+++ wordlist.clj
@@ -4,3 +4,12 @@
 ;; For example; if the list consisted only of the words "wow", "such",
 ;; "list" and "wow". Then the answer would be 3.
 
+(require '[clojure.string :as str])
+
+(->> (slurp "wordlist.dat")
+     (str/split-lines)
+     (into #{})
+     (count))
+
+;; => 109582
+
