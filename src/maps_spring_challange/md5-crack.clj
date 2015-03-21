6cdbc3faf41bd9d36b3d8bcf82a6bf1c3cc94a83
Lars Tveito <larstvei@ifi.uio.no>
2015-03-22 00:38:13 +0100

    Changed md5, speed improved!

--- src/maps_spring_challange/md5-crack.clj
+++ src/maps_spring_challange/md5-crack.clj
@@ -11,26 +11,20 @@
 
 (ns maps-spring-challenge.md5-crack
   (:require [clojure.string :as str]
-            [clojure.math.combinatorics :refer [cartesian-product]])
-  (:import  (java.security.MessageDigest)
-            (java.security.DigestInputStream)))
-
-(defn md5 [s]
-  (->> (.digest (doto (java.security.MessageDigest/getInstance "MD5")
-                  .reset (.update (.getBytes s))))
-       (map (partial format "%02x"))
-       (apply str)))
+            [clojure.math.combinatorics :refer [cartesian-product]]
+            [digest :as digest]))
 
 (time
- (let [alphabet (map #(char (+ 97 %)) (range 26))
+ (let [hash "3a8703f560b3768e0277094c58c686e1"
+       alphabet (map #(char (+ 97 %)) (range 26))
        passwords ((fn ! [n] (lazy-cat(->> alphabet
                                           (repeat n)
                                           (apply cartesian-product)
                                           (map #(apply str %)))
                                      (! (inc n)))) 1)]
    (->> passwords
-        (drop-while #(not= "3a8703f560b3768e0277094c58c686e1" (md5 %)))
+        (drop-while #(not= hash (digest/md5 %))) 
         (first))))
 
-;;    "Elapsed time: 107830.707635 msecs"
+;;    "Elapsed time: 12863.479869 msecs"
 ;; => "ghjkl"
