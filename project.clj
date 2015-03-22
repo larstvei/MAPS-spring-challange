6e8b412d47c973769cc77b5dc704e8cbe0c35ca3
Lars Tveito <larstvei@ifi.uio.no>
2015-03-22 13:52:00 +0100

    Solved Friday the 13th.

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/math.combinatorics "0.1.1"]
-                 [digest "1.4.4"]]
+                 [digest "1.4.4"]
+                 [clj-time "0.9.0"]]
   :resource-paths ["resources"])
--- src/maps_spring_challange/friday-the-13th.clj
+++ src/maps_spring_challange/friday-the-13th.clj
@@ -8,4 +8,21 @@
 ;; The answer should specify both of the consecutive months by year and
 ;; month number. For instance, for Feb and March 2015; 2015-02 2015-03
 
-(ns maps-spring-challenge.friday-the)
+(ns maps-spring-challange.friday-the-13th
+  (:require [clojure.string :as str]
+            [clj-time.core :as t]
+            [clj-time.predicates :as t?]
+            [clj-time.periodic :as tp]
+            [clj-time.format :as tf]))
+
+(let [format (tf/formatters :year-month)
+      days (-> (t/now)
+               (t/first-day-of-the-month)
+               (t/plus (t/days 12))
+               (tp/periodic-seq (t/months 1)))]
+  (loop [[t1 t2 & days] days]
+    (if (every? t?/friday? [t1 t2])
+      (->> [t1 t2] (map #(tf/unparse format %)) (str/join " " ))
+      (recur (cons t2 days)))))
+
+;; => "2026-02 2026-03"
